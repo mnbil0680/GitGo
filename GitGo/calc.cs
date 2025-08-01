aafdf387a8465c05c0b6b4b16e94765b92be6739
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-08-01 14:54:25 +0300

    add calc class

---
+++ GitGo/calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGo
+{
+    class calc
+    {
+        public int calculate(int age1, int age2)
+        {
+            return age1 + age2;
+        }
+
+    }
+}
